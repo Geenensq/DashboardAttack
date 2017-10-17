6a39913eb704b36f39c098d32eb4cf4f66310b91
QuentinGeenens <geenensq@gmail.com>
2017-10-17 22:23:05 +0200

    ok

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -28,8 +28,18 @@ class News extends CI_Controller
      */
     public function index()
     {
+        //--------------------------------Call base view---------------------------//
+        //--Here code for sessions------------------------------------------------//
         
-        // --------------------------------------- Add news instructions -----------------------------------------------------------------------//
+        //----Load view----------------------------------------------------------//
+        $this->load->view('/news/news.html');
+        //----------------------------------------------------------------------//
+    }
+
+
+    public function addnews()
+    {
+       // --------------------------------------- Add news instructions -----------------------------------------------------------------------//
         $this->form_validation->set_rules('autor', '"Nom de l\'auteur"', 'trim|required|min_length[1]|max_length[10]|alpha_dash|encode_php_tags');
         $this->form_validation->set_rules('title', '"titre de la news"', 'trim|required|min_length[1]|max_length[30]|alpha_dash|encode_php_tags');
         $this->form_validation->set_rules('content', '"contenu de la news"', 'trim|required|min_length[1]|max_length[255]|alpha_dash|encode_php_tags');
@@ -37,22 +47,17 @@ class News extends CI_Controller
             if ($this->form_validation->run())
             {
                 $addnews = $this->newsManager->ajouter_news($this->autor , $this->title , $this->content);
+                 $this->load->view('/news/news.html');
             }
-        //--------------------------------------------------------------------------------------------------------------------------------------//    
-        
-        
+        //--------------------------------------------------------------------------------------------------------------------------------------//   
+    }
+
+    public function deletenews()
+    {
         //--------------------------------------Delete news instructions------------------------------------------------------------------------//
-        
                $delnews = $this->newsManager->supprimer_news($this->id);
-    
-        //--------------------------------------------------------------------------------------------------------------------------------------//       
-
-
-        //--------------------------------Call base view---------------------------//
-        $this->load->view('/news/news.html');
-        //-------------------------------------------------------------------//
-
-
+                $this->load->view('/news/news.html');
+        //--------------------------------------------------------------------------------------------------------------------------------------//     
     }
     
 }
