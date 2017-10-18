03d4946975e8cffc811caa468c38db07bc1df1a3
stagiaire <geenensq@gmail.com>
2017-10-18 11:52:49 +0200

    Refactorisation

--- application/controllers/news_controller.php
+++ application/controllers/news_controller.php
@@ -1,44 +1,56 @@
 <?php
 
 class News_controller extends CI_Controller
-{  
-
+{ 
+    //------------------------------------CONSTRUCTOR CONTROLLER--------------------------// 
         function __construct()
         {
             parent::__construct();
-            $this->load->model('news_model' , 'newsManager');
+            //loading the model news
+            $this->load->model('news_model' , 'modelNews');
         }
+    //------------------------------------------------------------------------------------//    
 
+    //------------------------------------DEFAULT LOAD METHOD----------------------------//     
         public function index()
         {
-            //--------------------------------Call base view---------------------------//  
+           ///Loading my default view
             $this->load->view('/news/news.html');
-            //----------------------------------------------------------------------//
         }
+    //------------------------------------------------------------------------------------//  
+  
 
-        //--------------------------------------------------------------------------------------------------------------------------------------//
+    //----------------------------------METHOD FOR ADD AN NEWS WITH MY MODEL---------------//
         public function addnews() 
         { 
+                // Get _POST for my model
+                $this->modelNews->setAutor($this->input->post('autor'));
+                $this->modelNews->setTitle($this->input->post('title'));
+                $this->modelNews->setContent($this->input->post('content'));
+         
+                ///Launch method model for add an news 
+                $newsModel = $this->modelNews;
+                $addnews = $this->modelNews->ajouter_news($newsModel); 
 
-                // Get _POST for my model  
-                $auteur = $this->input->post('autor'); 
-                $titre = $this->input->post('title'); 
-                $contenu = $this->input->post('content'); 
-
-                ///Launch
-                $addnews = $this->newsManager->ajouter_news($auteur , $titre , $contenu); 
+                //Redirection to my default method
                 redirect(array('news_controller', 'index')); 
-             
         }            
-        //--------------------------------------------------------------------------------------------------------------------------------------//   
+    //------------------------------------------------------------------------------------//
     
 
+    //--------------------------METHOD FOR DELETE AN NEWS WITH MY MODEL------------------//   
         public function deletenews()
         {
-            //--------------------------------------Delete news instructions------------------------------------------------------------------------//
-                    $delnews = $this->newsManager->supprimer_news($this->input->post('id'));
-                    $this->load->view('/news/news.html');
-            //--------------------------------------------------------------------------------------------------------------------------------------//     
+                    // Get _POST for my model
+                    $this->modelNews->setId($this->input->post('id'));
+
+                    ///Launch method model delete my news by id 
+                    $newsModel = $this->modelNews;
+                    $delnews = $this->modelNews->supprimer_news($newsModel);
+
+                    //Redirection to my default method
+                     redirect(array('news_controller', 'index'));  
         }
+    //------------------------------------------------------------------------------------//     
     
 }
--- application/models/news_model.php
+++ application/models/news_model.php
@@ -14,11 +14,9 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
         public function __construct()
         {
-
         }
 
-        // Getters methods
-
+        // ---------------------------------------- Getters methods----------------------------------//
         public function getAutor()
         {
             return $this->autor;
@@ -38,8 +36,13 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
         {
             return $this->id;
         }
+        //-----------------------------------------------------------------------------------------------//
 
-        // Setters methods
+        //---------------------------------------Setters methods-----------------------------------------//
+        public function setId($id)
+        {
+            return $this->id = $id;
+        }
 
         public function setAutor($autor)
         {
@@ -55,65 +58,85 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
         {
             return $this->content = $content;
         }
+        //-----------------------------------------------------------------------------------------------//
 
-
-
-        public function ajouter_news($auteur, $titre, $contenu)
+        //-----------------------------PERSISTENCE METHOD FOR ADD MY NEWS-------------------------------//
+        public function ajouter_news($model)
         {
-            return $this->db->set('auteur', $auteur)->set('titre', $titre)->set('contenu', $contenu)->set('date_ajout', 'NOW()', false)->set('date_modif', 'NOW()', false)->insert($this->table);
+            $auteur = $model->getAutor();
+            $titre = $model->getTitle();
+            $contenu = $model->getContent();
+
+            return $this->db->set('autor', $auteur)
+                            ->set('title', $titre)
+                            ->set('content', $contenu)
+                            ->set('date_add', 'NOW()', false)
+                            ->set('date_modif', 'NOW()', false)
+                            ->insert($this->table);
         }
+        //-----------------------------------------------------------------------------------------------//
 
-
-        public function editer_news($id, $titre = null, $contenu = null)
+        //-----------------------------PERSISTENCE METHOD FOR DEL MY NEWS-------------------------------//
+        public function supprimer_news($model)
         {
-            //  Il n'y a rien à éditer
-            if ($titre == null AND $contenu == null)
-            {
-                return false;
-            }
-            //  Ces données seront échappées
-            if ($titre != null)
-            {
-                $this->db->set('titre', $titre);
-            }
+            $id = $model->getId();
 
-            if ($contenu != null)
-            {
-                $this->db->set('contenu', $contenu);
-            }
-            return $this->db->set('date_modif', 'NOW()', false)->where('id', (int)$id)->update($this->table);
-            }
+            return $this->db->where('id', (int)$id)
+                            ->delete($this->table);
+        }
+        //-----------------------------------------------------------------------------------------------//
+
+
+        // TODO
+        // 
+        // public function editer_news($id, $titre = null, $contenu = null)
+        // {
+        //     //  Il n'y a rien à éditer
+        //     if ($titre == null AND $contenu == null)
+        //     {
+        //         return false;
+        //     }
+        //     //  Ces données seront échappées
+        //     if ($titre != null)
+        //     {
+        //         $this->db->set('titre', $titre);
+        //     }
+
+        //     if ($contenu != null)
+        //     {
+        //         $this->db->set('contenu', $contenu);
+        //     }
+        //     return $this->db->set('date_modif', 'NOW()', false)->where('id', (int)$id)->update($this->table);
+        //     }
 
       
-        public function supprimer_news($id)
-        {
-            return $this->db->where('id', (int)$id)->delete($this->table);
-        }
+        
 
       
-        public function count($where = array())
-        {
-            return (int)$this->db->where($where)->count_all_results($this->table);
-        }
+        // public function count($where = array())
+        // {
+        //     return (int)$this->db->where($where)->count_all_results($this->table);
+        // }
 
         
-        public function liste_news($nb = 10, $debut = 0)
-        {
-            return $this->db->select('*')->from($this->table)->limit($nb, $debut)->order_by('id', 'desc')->get()->result();
+        // public function liste_news($nb = 10, $debut = 0)
+        // {
+        //     return $this->db->select('*')->from($this->table)->limit($nb, $debut)->order_by('id', 'desc')->get()->result();
                 
-        }
+        // }
 
-        public function getonebyid($id)
-        {
-                $query = $this->db->query('SELECT * FROM news WHERE `id` = ' . $id);
-                foreach ($query->result_array() as $row)
-                {
-                    echo $row['id'];
-                    echo $row['auteur'];
-                    echo $row['contenu'];
-                }
-            } 
-        }
+        // public function getonebyid($id)
+        // {
+        //         $query = $this->db->query('SELECT * FROM news WHERE `id` = ' . $id);
+        //         foreach ($query->result_array() as $row)
+        //         {
+        //             echo $row['id'];
+        //             echo $row['auteur'];
+        //             echo $row['contenu'];
+        //         }
+        // } 
+
+    }
 
 
 
