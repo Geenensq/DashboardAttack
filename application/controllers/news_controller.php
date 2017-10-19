f55ee701b289c85c77a2626bbb89ac27db9a1fbc
stagiaire <geenensq@gmail.com>
2017-10-19 14:05:07 +0200

    commit

--- application/controllers/news_controller.php
+++ application/controllers/news_controller.php
@@ -1,54 +1,60 @@
 <?php
 Class News_controller extends CI_Controller
 { 
-    //------------------------------------CONSTRUCTOR CONTROLLER--------------------------// 
+        //------------------------------------CONSTRUCTOR CONTROLLER--------------------------// 
         function __construct()
         {
             parent::__construct();
+            
             //loading the model news
             $this->load->model('news_model' , 'modelNews');
         }
-    //------------------------------------------------------------------------------------//    
+        //------------------------------------------------------------------------------------//    
 
-    //------------------------------------DEFAULT LOAD METHOD----------------------------//     
+        //------------------------------------index method---------------------------------------//     
         public function index()
         {
-            
-            //-------Loading all news for my table-------------//
+            // $test = debug();
             $listeNews = $this->modelNews->liste_news();
-            //------------------------------------------------//
-
-
-             //--------Loading my default view-----//
-             $this->load->view('/news/news.html',array('listeNews' =>$listeNews), false);
-            //------------------------------------// 
-            
-
-            
+            $this->load->view('/news/news.html', array('listeNews' =>$listeNews), false);
         }
-    //------------------------------------------------------------------------------------//  
+        //------------------------------------------------------------------------------------//  
   
-    //----------------------------------METHOD FOR ADD AN NEWS WITH MY MODEL---------------//
+        //----------------------------------method to add news with my model-----------------//
         public function addNews() 
         { 
-                // Get _POST for my model
-                $this->modelNews->setAutor($this->input->post('autor'));
-                $this->modelNews->setTitle($this->input->post('title'));
-                $this->modelNews->setContent($this->input->post('content'));
-         
-                ///Launch method model for add an news 
-                $newsModel = $this->modelNews;
-                $addNews = $this->modelNews->ajouter_News($newsModel); 
+                //setting rules with the form validation library
+                $this->form_validation->set_rules('autor', 'autor of the news', 'encode_php_tags|trim|required|max_length[20]');
+                $this->form_validation->set_rules('title', 'title of the news', 'encode_php_tags|trim|required|max_length[70]');
+                $this->form_validation->set_rules('content', 'content of the news', 'encode_php_tags|trim|required|max_length[500]');   
 
-                //Redirection to my default method
-                redirect(array('news_controller', 'index')); 
+                 if ($this->form_validation->run() == FALSE)
+                {
+                            //Redirect to the default method if the form is not filled correctly
+                            redirect(array('news_controller', 'index')); 
+                }
+                else
+                {
+                            //Else creating my object with my setter and POST
+                            $this->modelNews->setAutor($this->input->post('autor'));
+                            $this->modelNews->setTitle($this->input->post('title'));
+                            $this->modelNews->setContent($this->input->post('content'));
+                     
+                            ///Launch method model for add an news 
+                            $newsModel = $this->modelNews;
+                            $addNews = $this->modelNews->ajouter_News($newsModel); 
+
+                            //Redirection to my default method
+                            redirect(array('news_controller', 'index')); 
+                            }
         }            
-    //------------------------------------------------------------------------------------//
+        //------------------------------------------------------------------------------------//
     
-
-    //--------------------------METHOD FOR DELETE AN NEWS WITH MY MODEL------------------//   
+        //--------------------------method to delete a news based on its id with my model------------------//   
         public function deleteNews()
-        {
+        {   
+                if (isset($_POST['id'])){
+
                     // Get _POST for my model
                     $this->modelNews->setId($this->input->post('id'));
 
@@ -58,9 +64,19 @@ Class News_controller extends CI_Controller
 
                     //Redirection to my default method
                      redirect(array('news_controller', 'index'));  
-        }
-    //------------------------------------------------------------------------------------//  
-    
 
-    
+                }  elseif (isset($_GET['id'])) {
+
+                    $this->modelNews->setId($_GET['id']) ;
+
+                    ///Launch method model delete my news by id 
+                    $newsModel = $this->modelNews;
+                    $delnews = $this->modelNews->supprimer_news($newsModel);
+
+                    //Redirection to my default method
+                     redirect(array('news_controller', 'index'));  
+                }
+                          
+        }
+        //------------------------------------------------------------------------------------//     
 }
