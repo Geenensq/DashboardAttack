c72ae7df1dc4337ef3b7ebe6cd6ae58cf400cb89
QuentinGeenens <geenensq@gmail.com>
2017-11-21 12:09:05 +0100

    refacto

--- application/helpers/assets_helper.php
+++ application/helpers/assets_helper.php
@@ -4,40 +4,48 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 if ( ! function_exists('css_url'))
 {
-    function css_url($nom)
+    function css_url($name)
     {
-        return base_url() . 'assets/css/' . $nom . '.css';
+        return base_url() . 'assets/css/' . $name . '.css';
+    }
+}
+
+if ( ! function_exists('css_url_lib'))
+{
+    function css_url_lib($name)
+    {
+        return base_url() . 'assets/css/lib/' . $name . '.css';
     }
 }
 
 if ( ! function_exists('js_url'))
 {
-    function js_url($nom)
+    function js_url($name)
     {
-        return base_url() . 'assets/js/' . $nom . '.js';
+        return base_url() . 'assets/js/' . $name . '.js';
     }
 }
 
 if ( ! function_exists('js_url_lib'))
 {
-    function js_url_lib($nom)
+    function js_url_lib($name)
     {
-        return base_url() . 'assets/js/lib/' . $nom . '.js';
+        return base_url() . 'assets/js/lib/' . $name . '.js';
     }
 }
 
 if ( ! function_exists('img_url'))
 {
-    function img_url($nom)
+    function img_url($name)
     {
-        echo base_url() . 'assets/img/' . $nom;
+        echo base_url() . 'assets/img/' . $name;
     }
 }
 
 if ( ! function_exists('img'))
 {
-    function img($nom, $alt = '')
+    function img($name, $alt = '')
     {
-        return '<img src="' . img_url($nom) . '" alt="' . $alt . '" />';
+        return '<img src="' . img_url($name) . '" alt="' . $alt . '" />';
     }
 }
