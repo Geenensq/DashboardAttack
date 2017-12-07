9c200f20c920c7d3ce616c92803903be7383e03f
QuentinGeenens <geenensq@gmail.com>
2017-12-07 21:28:02 +0100

    test ajax auto complete

--- application/controllers/customers_controller.php
+++ application/controllers/customers_controller.php
@@ -175,6 +175,33 @@ Class Customers_controller extends CI_Controller
 // =======================================================================//
 // !       Method GET GROUPS CUSTOMERS FOR AUTOCOMPLETE FUNCTION          //
 // ======================================================================//
+
+    public function fetch()
+    {
+
+    define ("DB_USER", "root");
+    define ("DB_PASSWORD", "");
+    define ("DB_DATABASE", "testdb");
+    define ("DB_HOST", "localhost");
+
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+    $sql = "SELECT name FROM groups_customers 
+            WHERE name LIKE '%".$_GET['query']."%'
+            LIMIT 10"; 
+    $result = $mysqli->query($sql);
+    
+    $json = [];
+    while($row = $result->fetch_assoc()){
+         $json[] = $row['name'];
+    }
+
+    echo json_encode($json);
+
+
+
 }
+
+ }
 
 ?>
