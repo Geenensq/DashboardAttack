a7ea409f640ad5a4201618adeaf0d3f88840c7a7
stagiaire <geenensq@gmail.com>
2017-12-08 11:41:37 +0100

    Finish features for add an customers

--- application/controllers/customers_controller.php
+++ application/controllers/customers_controller.php
@@ -178,8 +178,24 @@ Class Customers_controller extends CI_Controller
         echo json_encode($callBack);
     }
 
+    public function fetch()
+    {
+        define ("DB_USER", "root");
+        define ("DB_PASSWORD", "");
+        define ("DB_DATABASE", "testdb");
+        define ("DB_HOST", "localhost");
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+        $sql = "SELECT name FROM groups_customers 
+            WHERE name LIKE '%".$_GET['query']."%'
+            LIMIT 10";
+        $result = $mysqli->query($sql);
 
-
+        $json = [];
+        while($row = $result->fetch_assoc()){
+            $json[] = $row['name'];
+        }
+        echo json_encode($json);
+    }
 
 
 
